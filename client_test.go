fc5540e96a114ce51ffba5c44193ba73e47db996
mitz <mitz@cybozu.co.jp>
2018-08-06 00:30:53 +0000

    Define NewClient() and separate client func to client.go

---
+++ client_test.go
@@ -0,0 +1,76 @@
+package etcdutil
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+const (
+	etcdClientURL = "http://localhost:12379"
+	etcdPeerURL   = "http://localhost:12380"
+)
+
+func testMain(m *testing.M) int {
+	circleci := os.Getenv("CIRCLECI") == "true"
+	if circleci {
+		code := m.Run()
+		os.Exit(code)
+	}
+
+	etcdPath, err := ioutil.TempDir("", "etcdutil-test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("etcd",
+		"--data-dir", etcdPath,
+		"--initial-cluster", "default="+etcdPeerURL,
+		"--listen-peer-urls", etcdPeerURL,
+		"--initial-advertise-peer-urls", etcdPeerURL,
+		"--listen-client-urls", etcdClientURL,
+		"--advertise-client-urls", etcdClientURL)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		cmd.Process.Kill()
+		cmd.Wait()
+		os.RemoveAll(etcdPath)
+	}()
+
+	return m.Run()
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(testMain(m))
+}
+
+func testEtcdClient(t *testing.T) {
+	t.Parallel()
+
+	var clientURL string
+	circleci := os.Getenv("CIRCLECI") == "true"
+	if circleci {
+		clientURL = "http://localhost:2379"
+	} else {
+		clientURL = etcdClientURL
+	}
+
+	cfg := NewConfig()
+	cfg.Prefix = t.Name() + "/"
+	cfg.Endpoints = []string{clientURL}
+	_, err := NewClient(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestEtcdutilClient(t *testing.T) {
+	t.Run("etcdClient", testEtcdClient)
+}
